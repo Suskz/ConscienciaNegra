695c4fd1b170106555f33711b241e0e54d1e3645
Suskz <ielsondias1@gmail.com>
2024-11-26 16:42:44 -0300

    hello

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Exibe uma janela de alerta quando a página é carregada
+    alert("Bem-vindo ao site sobre a Consciência Negra!");
+
+});
